1a95688c4e791a52dd011c864451b56883a0992a
Laura Dengra Torres <laura.dengra.torres@thinktecture.com>
2024-04-23 08:05:07 +0200

    lab 12 - router link active fix list styling

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   providers: [],
 })
 export class AppComponent {
