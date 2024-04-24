6fe615814f300c9d05a73c919e218658c2c11747
Laura Dengra Torres <laura.dengra.torres@thinktecture.com>
2024-04-24 12:37:38 +0200

    lab 1 und 2 - bindings

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -6,8 +6,23 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'todo';
+  computedField = false;
+  computedNumber = 300;
+
+  value = 'Hallo';
+
+  myColor = 'green';
+
+  afterClicked(event: MouseEvent) {
+    console.log(event.clientX);
+    console.log(event.clientY);
+  }
+
+  movedMyMouse() {
+    console.log('Hallo!');
+  }
 }
