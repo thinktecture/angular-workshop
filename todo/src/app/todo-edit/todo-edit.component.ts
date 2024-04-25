6d71f03fc3b82cc7b7e8de588f0cb71f75d0188c
Laura Dengra Torres <laura.dengra.torres@thinktecture.com>
2024-04-25 16:14:04 +0200

    lab 12 - template forms + bmi calculator

--- todo/src/app/todo-edit/todo-edit.component.ts
+++ todo/src/app/todo-edit/todo-edit.component.ts
@@ -3,22 +3,31 @@ import { ActivatedRoute } from '@angular/router';
 import { TodoService } from '../todo.service';
 import { map, switchMap } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todo-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo-edit.component.html',
-  styleUrl: './todo-edit.component.scss'
+  styleUrl: './todo-edit.component.scss',
 })
 export class TodoEditComponent {
+  bodySizeInMeter = 0;
+  weightInKg = 0;
+
   protected readonly todo$ = this.activatedRoute.params.pipe(
-    map(params => +params['id']),
-    switchMap(id => this.todoService.get(id)),
+    map((params) => +params['id']),
+    switchMap((id) => this.todoService.get(id))
   );
 
   constructor(
     private readonly todoService: TodoService,
-    private readonly activatedRoute: ActivatedRoute,
+    private readonly activatedRoute: ActivatedRoute
   ) {}
+
+  onSubmit(todo: Todo) {
+    this.todoService.update(todo).subscribe();
+  }
 }
