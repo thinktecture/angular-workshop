97ec93d1b651516b6d292f6f3ec0312d2619baed
Christian Liebel <christian.liebel@thinktecture.com>
2024-04-25 16:51:44 +0200

    Lab #13

--- todo/src/app/todo-create/todo-create.component.ts
+++ todo/src/app/todo-create/todo-create.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TodoService } from '../todo.service';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-todo-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo-create.component.html',
   styleUrl: './todo-create.component.scss'
 })
 export class TodoCreateComponent {
+  private readonly fb = inject(NonNullableFormBuilder);
+  private readonly todoService = inject(TodoService);
+  protected readonly formGroup = this.fb.group({
+    name: ['test', [Validators.required, Validators.minLength(3)]],
+    done: [false],
+  });
+  
+  constructor() {
+    this.formGroup.valueChanges
+    .pipe(debounceTime(300))
+    .subscribe(value => console.log(value));
+  }
 
+  onSubmit() {
+    this.todoService
+    .create(this.formGroup.getRawValue())
+    .subscribe();
+  }
 }
